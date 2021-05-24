b09edd2e487fd79b10594e1156520b40f93181f0
casder-succ <casderiopus@gmail.com>
2021-05-25 02:54:10 +0300

    some

--- scripts/sendreview.js
+++ scripts/sendreview.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $("#sendMail").click(function() {
+    $("#sendMail").click(function(event) {
+        event.preventDefault()
+
         if($("#email").val() === '') {
             $("#email").css("border-color", "#f00");
             return false;
@@ -28,7 +30,7 @@ $(document).ready(function () {
 
         $.ajax({
             type:"POST",
-            url:"review.php",
+            url:"scripts/review.php",
             cache: false,
             data:$("#review-form").serialize(),
             success: function(result) {
